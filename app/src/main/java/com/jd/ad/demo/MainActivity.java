931df4bb953d1e74fb7e9176003e791a9cd177cb
haorongrong3 <haorongrong3@jd.com>
2022-07-12 10:22:20 +0800

    add multi sdk

--- app/src/main/java/com/jd/ad/demo/MainActivity.java
+++ app/src/main/java/com/jd/ad/demo/MainActivity.java
@@ -1,6 +1,8 @@
-package com.jd.ad;
+package com.jd.ad.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
